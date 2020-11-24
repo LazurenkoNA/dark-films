dd0e05928b6f515976dbf96c8044bff2d8b8e384
LazurenkoNA <naj040315@gmail.com>
2020-11-24 10:51:19 +0200

    end initialization 2.0

---
+++ src/animations/animations.js
@@ -0,0 +1,18 @@
+// eslint-disable-next-line import/prefer-default-export
+export const fadeIn = {
+  hidden: {
+    opacity: 0,
+  },
+  show: {
+    opacity: 1,
+    transition: {
+      duration: 0.75,
+    },
+  },
+  exit: {
+    opacity: 0,
+    transition: {
+      duration: 0.75,
+    },
+  },
+};
