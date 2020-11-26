41a9cb32ad14d11bd2672b8412328c5c85cbc3f0
raydenker <raydenkerlarin@gmail.com>
2020-11-26 17:35:37 +0200

    Add slider to user

--- src/components/SliderCategory/SliderCategory.js
+++ src/components/SliderCategory/SliderCategory.js
@@ -1,5 +1,35 @@
 import React from 'react';
+import SwiperCore, { Navigation } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import data from '../../data';
+// Import Swiper styles
+import 'swiper/swiper.scss';
+import 'swiper/components/navigation/navigation.scss';
 
-export default function SliderCategory() {
-  return <div />;
-}
+SwiperCore.use([Navigation]);
+console.log(data);
+const SliderCategory = () => {
+  return (
+    <div>
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={4}
+        navigation
+        scrollbar={{ draggable: true }}
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}
+        // onSwiper={(swiper) => console.log(data)}
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+      </Swiper>
+    </div>
+  );
+};
+export default SliderCategory;
---
+++ src/data.js
@@ -0,0 +1,115 @@
+const data = [
+  {
+    img: 'https://source.unsplash.com/random',
+    title: 'Movie Test Name',
+    date: '2003-01-05',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At cumque dolorum enim excepturi incidunt molestiae nesciunt odio qui quidem reprehenderit.',
+  },
+  {
+    img: 'https://source.unsplash.com/random',
+    title: 'Movie Test Name',
+    date: '2003-01-05',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At cumque dolorum enim excepturi incidunt molestiae nesciunt odio qui quidem reprehenderit.',
+  },
+  {
+    img: 'https://source.unsplash.com/random',
+    title: 'Movie Test Name',
+    date: '2003-01-05',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At cumque dolorum enim excepturi incidunt molestiae nesciunt odio qui quidem reprehenderit.',
+  },
+  {
+    img: 'https://source.unsplash.com/random',
+    title: 'Movie Test Name',
+    date: '2003-01-05',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At cumque dolorum enim excepturi incidunt molestiae nesciunt odio qui quidem reprehenderit.',
+  },
+  {
+    img: 'https://source.unsplash.com/random',
+    title: 'Movie Test Name',
+    date: '2003-01-05',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At cumque dolorum enim excepturi incidunt molestiae nesciunt odio qui quidem reprehenderit.',
+  },
+  {
+    img: 'https://source.unsplash.com/random',
+    title: 'Movie Test Name',
+    date: '2003-01-05',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At cumque dolorum enim excepturi incidunt molestiae nesciunt odio qui quidem reprehenderit.',
+  },
+  {
+    img: 'https://source.unsplash.com/random',
+    title: 'Movie Test Name',
+    date: '2003-01-05',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At cumque dolorum enim excepturi incidunt molestiae nesciunt odio qui quidem reprehenderit.',
+  },
+  {
+    img: 'https://source.unsplash.com/random',
+    title: 'Movie Test Name',
+    date: '2003-01-05',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At cumque dolorum enim excepturi incidunt molestiae nesciunt odio qui quidem reprehenderit.',
+  },
+  {
+    img: 'https://source.unsplash.com/random',
+    title: 'Movie Test Name',
+    date: '2003-01-05',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At cumque dolorum enim excepturi incidunt molestiae nesciunt odio qui quidem reprehenderit.',
+  },
+  {
+    img: 'https://source.unsplash.com/random',
+    title: 'Movie Test Name',
+    date: '2003-01-05',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At cumque dolorum enim excepturi incidunt molestiae nesciunt odio qui quidem reprehenderit.',
+  },
+  {
+    img: 'https://source.unsplash.com/random',
+    title: 'Movie Test Name',
+    date: '2003-01-05',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At cumque dolorum enim excepturi incidunt molestiae nesciunt odio qui quidem reprehenderit.',
+  },
+  {
+    img: 'https://source.unsplash.com/random',
+    title: 'Movie Test Name',
+    date: '2003-01-05',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At cumque dolorum enim excepturi incidunt molestiae nesciunt odio qui quidem reprehenderit.',
+  },
+  {
+    img: 'https://source.unsplash.com/random',
+    title: 'Movie Test Name',
+    date: '2003-01-05',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At cumque dolorum enim excepturi incidunt molestiae nesciunt odio qui quidem reprehenderit.',
+  },
+  {
+    img: 'https://source.unsplash.com/random',
+    title: 'Movie Test Name',
+    date: '2003-01-05',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At cumque dolorum enim excepturi incidunt molestiae nesciunt odio qui quidem reprehenderit.',
+  },
+  {
+    img: 'https://source.unsplash.com/random',
+    title: 'Movie Test Name',
+    date: '2003-01-05',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At cumque dolorum enim excepturi incidunt molestiae nesciunt odio qui quidem reprehenderit.',
+  },
+  {
+    img: 'https://source.unsplash.com/random',
+    title: 'Movie Test Name',
+    date: '2003-01-05',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At cumque dolorum enim excepturi incidunt molestiae nesciunt odio qui quidem reprehenderit.',
+  },
+];
+export default data;
