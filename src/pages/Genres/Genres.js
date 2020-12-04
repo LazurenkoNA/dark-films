0ef7e8df38f9cbf4fcb5b5113bf495fbf07f5649
LazurenkoNA <naj040315@gmail.com>
2020-12-04 12:40:40 +0200

    decomposition logic

--- src/pages/Genres/Genres.js
+++ src/pages/Genres/Genres.js
@@ -8,13 +8,13 @@ const Genres = () => {
   // Get Data
   const { genres, nowPlaying, movieByGenre } = useSelector((state) => state.movies);
   const { nameGenre } = useSelector((state) => state.currentGenre);
-
+  
   return (
     <div style={{ paddingTop: '6rem' }}>
-      {nowPlaying && <SliderSection content={nowPlaying} title="Now Playing" />}
-
-      {genres && <GenresButtons content={genres} title="Genres" />}
-
+      {nowPlaying && <SliderSection content={nowPlaying} title='Now Playing' />}
+  
+      {genres && <GenresButtons content={genres} title='Genres' />}
+  
       {movieByGenre && <CardSection content={movieByGenre} titleSection={nameGenre} />}
     </div>
   );
